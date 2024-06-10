fa99641180d525eb09c7519f51b6658f2d41e117
hatmix <67128187+hatmix@users.noreply.github.com>
2024-06-10 14:52:30 -0400

    Hide exit button on web

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -6,3 +6,6 @@ func _ready() -> void:
 	%Settings.pressed.connect(GuiTransitions.go_to.bind("Settings"))
 	%Credits.pressed.connect(GuiTransitions.go_to.bind("Credits"))
 	%Exit.pressed.connect(get_tree().call_deferred.bind("quit"))
+
+	if OS.get_name() == "Web":
+		%Exit.hide()
