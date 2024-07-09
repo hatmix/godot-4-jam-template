5c860b176e0e09c67e0a63930867f8aadcdc860b
hatmix <67128187+hatmix@users.noreply.github.com>
2024-07-09 10:33:34 -0400

    Attempt no-cors itch.io api request

--- main.gd
+++ main.gd
@@ -3,4 +3,11 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.request("https://itch.io/api/1/me/me", ["mode: no-cors"])
 	GuiTransitions.go_to("MainMenu")
+
+func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	var response_json = JSON.parse_string(body.get_string_from_utf8())
+	print(response_json)
+
