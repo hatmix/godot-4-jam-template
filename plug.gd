888de2a23463a521da7a4a18970c0368e6f5b1d8
hatmix <67128187+hatmix@users.noreply.github.com>
2024-07-10 08:25:06 -0400

    Bump simple-gui-transitions to v0.4.0

--- plug.gd
+++ plug.gd
@@ -5,6 +5,6 @@ func _plugging():
 	plug("bitwes/Gut", {"tag": "v9.2.1", "dev": true})
 	plug("Maaack/Godot-UI-Sound-Controller", {"tag": "v0.5.0"})
 	plug("daenvil/MarkdownLabel", {"tag": "v1.2.0"})
-	plug("murikistudio/simple-gui-transitions", {"tag": "v0.2.2", "exclude": ["example"]})
+	plug("murikistudio/simple-gui-transitions", {"tag": "v0.4.0", "exclude": ["example"]})
 	plug("derkork/godot-resource-groups", {"tag": "v0.3.0", "exclude": ["csharp"]})
 	plug("hatmix/godot-input-event-icons", {"exclude": ["gd-plug"]})
