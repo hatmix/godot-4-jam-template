37d279f7d027de6c907802e4a81a84b0200d4399
hatmix <67128187+hatmix@users.noreply.github.com>
2024-12-27 13:44:41 -0500

    Bump addon godot-resource-groups to v0.4.1

--- plug.gd
+++ plug.gd
@@ -8,5 +8,5 @@ func _plugging():
 	plug("Maaack/Godot-UI-Sound-Controller", {"tag": "v0.5.0"})
 	plug("daenvil/MarkdownLabel", {"tag": "v1.2.0"})
 	plug("murikistudio/simple-gui-transitions", {"tag": "v0.4.0", "exclude": ["example"]})
-	plug("derkork/godot-resource-groups", {"tag": "v0.3.0", "exclude": ["csharp"]})
+	plug("derkork/godot-resource-groups", {"tag": "v0.4.1", "exclude": ["csharp"]})
 	plug("hatmix/godot-input-event-icons", {"exclude": ["gd-plug"]})
