f7442c2757e2b5faf21635a0a9a24c586c645767
hatmix <67128187+hatmix@users.noreply.github.com>
2025-01-20 12:36:49 -0500

    Add how to play screen

---
+++ HOW_TO_PLAY.md
@@ -0,0 +1,3 @@
+# How to Play
+
+A markdown doc with instructions for the player.
---
+++ ui/how_to_play/how_to_play.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var ui: Node
+
+
+func _ready() -> void:
+	%MarkdownLabel.display_file("res://HOW_TO_PLAY.md")
+	call_deferred("_connect_buttons")
+
+
+func _connect_buttons() -> void:
+	await get_tree().process_frame
+	%Back.pressed.connect(ui.go_to.bind("MainMenu"))
+
+
+func _input(_event: InputEvent) -> void:
+	if visible and Input.is_action_just_pressed("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		ui.go_to("MainMenu")
