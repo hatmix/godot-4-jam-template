445a3c26a9cff854f94fbbb2a51fb502f4a7b9cd
hatmix <67128187+hatmix@users.noreply.github.com>
2025-01-30 06:31:47 -0500

    Add game_ui TODO

--- ui/game/game_ui.gd
+++ ui/game/game_ui.gd
@@ -2,6 +2,7 @@ extends Control
 
 var ui: Node
 
+# TODO: Replace game_ui.tscn and game_ui.gd with your game's UI/HUD
 
 func _input(_event: InputEvent) -> void:
 	if visible and Input.is_action_just_pressed("ui_cancel"):
