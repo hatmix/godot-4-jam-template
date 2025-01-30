50a192332280c925c7885ab298753c1ce5638dd8
hatmix <67128187+hatmix@users.noreply.github.com>
2025-01-30 12:16:17 -0500

    Remove YourBuil addon

--- ui/main_menu/git_info.gd
+++ ui/main_menu/git_info.gd
@@ -3,21 +3,7 @@ extends GridContainer
 @onready var build_value: Label = $BuildValue
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var tag: String = YourBuil.git_tag
-	var count: int = YourBuil.git_commit_count
-	var hash: String = YourBuil.git_commit_hash
-	if tag:
-		build_value.text = tag
-	elif count:
-		build_value.text = str(count)
-		if hash:
-			build_value.text += ".%s" % hash.right(6)
-	else:
-		self.hide()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Just hide until a versioning system is in place
+	# Not sure why YourBuil doesn't work in the container, but it doesn't
+	self.hide()
