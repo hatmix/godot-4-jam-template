61c9ac3f0194d5e49e679c25832b66981c41ef4b
hatmix <67128187+hatmix@users.noreply.github.com>
2025-02-23 17:51:59 -0500

    Add thanks for playing screen

---
+++ ui/thanks_for_playing/thanks_for_playing.gd
@@ -0,0 +1,14 @@
+extends UiPage
+
+@export var thank_you_image: Texture2D:
+	set(texture):
+		thank_you_image = texture
+		texture_rect.texture = thank_you_image
+		
+@onready var texture_rect: TextureRect = $TextureRect
+
+
+func _process(_delta: float) -> void:
+	if visible and Input.is_anything_pressed():
+		await ui.hide_ui(self)
+		get_tree().change_scene_to_file("res://main.tscn")
