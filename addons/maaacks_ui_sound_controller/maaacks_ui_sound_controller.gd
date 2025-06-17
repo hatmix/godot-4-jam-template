eb8be21357afeb4062a9b92dbc17849b38cf7cd2
hatmix <67128187+hatmix@users.noreply.github.com>
2025-06-17 16:18:42 -0400

    Remove ui sound controller

--- addons/maaacks_ui_sound_controller/maaacks_ui_sound_controller.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends EditorPlugin
-
-
-func _enter_tree():
-	add_autoload_singleton("ProjectUISoundController", "res://addons/maaacks_ui_sound_controller/base/scenes/Autoloads/ProjectUISoundController.tscn")
-
-func _exit_tree():
-	remove_autoload_singleton("ProjectUISoundController")
