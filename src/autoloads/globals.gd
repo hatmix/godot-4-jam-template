04636f1d9d15f95ee9ab2e911132a9fcd56a7af0
hatmix <67128187+hatmix@users.noreply.github.com>
2025-11-12 08:41:35 -0500

    Remove utils.gd and other cleanup

--- src/autoloads/globals.gd
+++ src/autoloads/globals.gd
@@ -1,9 +1,5 @@
 extends Node
 
-# What goes into globals.gd?
-# If the function depends on the something in the game, it's a global.
-# If it's independent, it (probably) belongs in utils.gd
-
 ## Use UI/MessageBox to display a status update message to the player
 @warning_ignore("unused_signal")
 signal post_ui_message(text: String)
