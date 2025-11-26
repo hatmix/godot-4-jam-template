aeb3bdb456e4f4c245ec89bd5904dfd65c903806
hatmix <67128187+hatmix@users.noreply.github.com>
2025-11-26 09:14:24 -0500

    Skip ui/settings test in headless

--- test/ui/settings_test/settings_test.gd
+++ test/ui/settings_test/settings_test.gd
@@ -4,9 +4,9 @@ extends GdUnitTestSuite
 @warning_ignore("unused_parameter")
 @warning_ignore("return_value_discarded")
 
-
-# Ensure the UI "page" can be run directly for development convenience
-func test_as_run_current_scene() -> void:
+# Need to skip settings under headless testing, as it uses get_viewport()
+# Ensure the UiPage can be run directly for development convenience
+func test_as_run_current_scene(_do_skip := DisplayServer.get_name() == "headless") -> void:
 	var runner: GdUnitSceneRunner = scene_runner("res://src/ui/settings/settings.tscn")
 	# Because there is an autoload name "Settings", the "Settings" page will get a different name
 	# when added to the tree on its own.
