90de6cdc0b3156f5bde6188aed6e305e665fceaa
hatmix <67128187+hatmix@users.noreply.github.com>
2025-11-26 11:48:48 -0500

    Skip ui/settings test under X11 (for CI)

--- test/ui/settings_test/settings_test.gd
+++ test/ui/settings_test/settings_test.gd
@@ -3,17 +3,17 @@ class_name SettingsTest
 extends GdUnitTestSuite
 
 var is_headless: bool = DisplayServer.get_name().contains("headless")
-
+var is_ci: bool = DisplayServer.get_name() == ("X11")
 
 # Need to skip settings under headless testing, as it uses get_viewport()
 @warning_ignore("inferred_declaration")
-func before(_do_skip := is_headless) -> void:
+func before(_do_skip := is_ci) -> void:
 	pass
 
 
 # Ensure the UiPage can be run directly for development convenience
 @warning_ignore("inferred_declaration")
-func test_as_run_current_scene(_do_skip := is_headless) -> void:
+func test_as_run_current_scene(_do_skip := is_ci) -> void:
 	var runner: GdUnitSceneRunner = scene_runner("res://src/ui/settings/settings.tscn")
 	# Because there is an autoload name "Settings", the "Settings" page will get a different name
 	# when added to the tree on its own.
