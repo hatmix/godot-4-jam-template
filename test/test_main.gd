0969f29b1d3496cdf419d293e4546f63d1e4c587
hatmix <67128187+hatmix@users.noreply.github.com>
2026-07-19 12:57:56 -0400

    Fix main.tscn added by merge from template

--- test/test_main.gd
+++ test/test_main.gd
@@ -3,5 +3,5 @@ extends GdUnitTestSuite
 
 
 func test_main():
-	var runner: GdUnitSceneRunner = scene_runner("res://main.tscn")
+	var runner: GdUnitSceneRunner = scene_runner("res://src/main.tscn")
 	assert_str(runner.scene().name).is_equal("Main")
